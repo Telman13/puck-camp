7361065c7bdac781343486ffd512e055111e6c59
Telman13 <teliyev10@std.beu.edu.az>
2024-10-31 01:28:45 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+    <>
+      <Button>Hurby</Button>
+    </>
+  )
+}
+
+export default App
